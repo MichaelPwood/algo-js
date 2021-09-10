576c4451a33f6b03284bca2e869bb52fa33b8a0c
Michael <pelzer.michael.05@gmail.com>
2021-09-10 14:12:46 +0200

    Exercice 1.6

---
+++ 1.6.js
@@ -0,0 +1,8 @@
+function nombreEntier() {
+  let nbrEntier01 = Number(document.getElementById("nbrEntier01").value);
+  let nbrEntier02 = Number(document.getElementById("nbrEntier02").value);
+  let totalDivision;
+  totalDivision = nbrEntier01 / nbrEntier02;
+  document.getElementById("division").innerHTML =
+    "La division de vos deux nombres est : " + totalDivision + " !";
+}
