7a25287ec356b428cf156741ae4c0b9bd26d9587
Michael <pelzer.michael.05@gmail.com>
2021-09-10 16:29:25 +0200

    exercice 2.2

--- 1.6.js
+++ 1.6.js
@@ -2,7 +2,7 @@ function nombreEntier() {
   let nbrEntier01 = Number(document.getElementById("nbrEntier01").value);
   let nbrEntier02 = Number(document.getElementById("nbrEntier02").value);
   let totalDivision;
-  totalDivision = nbrEntier01 / nbrEntier02;
+  totalDivision = nbrEntier01 % nbrEntier02;
   document.getElementById("division").innerHTML =
-    "La division de vos deux nombres est : " + totalDivision + " !";
+    "Le reste de la division de vos deux nombres est : " + totalDivision + " !";
 }
