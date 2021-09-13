4020b48c7fecd5f8948a7775351e00fbd068e3ff
Michael <pelzer.michael.05@gmail.com>
2021-09-13 10:05:30 +0200

    Exercice 3.1

---
+++ 3.1.js
@@ -0,0 +1,14 @@
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [100, 101, 102];
+let res;
+
+for (let i = 0; i < arr1.length; i++) {
+    res = arr1[i];
+}
+console.log(res);
+for (let i = 0; i < arr2.length; i++) {
+    res = arr2[i];
+}
+console.log(res);
+
+  
